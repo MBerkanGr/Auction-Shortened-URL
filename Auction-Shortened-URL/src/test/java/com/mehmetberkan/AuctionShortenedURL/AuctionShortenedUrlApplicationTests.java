38cca3b22d56031c84c75574cca8864981a1f27a
Mehmet Berkan <mehmetberkangorel@gmail.com>
2021-12-14 21:49:58 +0300

    Initial commit

---
+++ Auction-Shortened-URL/src/test/java/com/mehmetberkan/AuctionShortenedURL/AuctionShortenedUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mehmetberkan.AuctionShortenedURL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionShortenedUrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
